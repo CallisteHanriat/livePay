59d30ed12396c847976398be27a77af8ccb2dfef
Calliste Hanriat <couim38@gmail.com>
2015-07-30 22:41:07 +0200

    Add some variables into interfaceGui

--- livePayProject/src/gui/InterfaceGui.java
+++ livePayProject/src/gui/InterfaceGui.java
@@ -1,7 +1,11 @@
 package gui;
 
 import files.*;
+import java.awt.Dimension;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,15 +17,23 @@ import javax.swing.JFrame;
  * @author couim
  */
 public class InterfaceGui extends JFrame {
+    private JSpinner inputSalaire;
+    private JSpinner inputTempsIntervalle;
+    private JSpinner inputActu;
+    
     public InterfaceGui() {
-        
+        this.initUiComponant();
     }
     
     /**
      * Use it to init all ui componant to the GUI.
      */
     private void initUiComponant() {
+        this.add(new JLabel("Salaire : "));
         
+        this.add(new JLabel("par : "));
+        this.setPreferredSize(new Dimension(500,400));
+        this.setVisible(true);
     }
     
     //GETTERS
