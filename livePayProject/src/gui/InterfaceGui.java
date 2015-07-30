fc58fb4c8cfbf710721983e8f0007afdd74ab302
Calliste Hanriat <couim38@gmail.com>
2015-07-30 14:01:49 +0200

    Add some methods in interfaceGUI

--- livePayProject/src/gui/InterfaceGui.java
+++ livePayProject/src/gui/InterfaceGui.java
@@ -1,6 +1,7 @@
 package gui;
 
 import files.*;
+import javax.swing.JFrame;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,6 +12,25 @@ import files.*;
  *
  * @author couim
  */
-public class InterfaceGui {
+public class InterfaceGui extends JFrame {
+    public InterfaceGui() {
+        
+    }
     
+    /**
+     * Use it to init all ui componant to the GUI.
+     */
+    private void initUiComponant() {
+        
+    }
+    
+    //GETTERS
+    public void getUi() {
+        
+    }
+    
+    //SETTERS
+    public void setUi() {
+        
+    }
 }
